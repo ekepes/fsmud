5a52dfa626ac79a1a4261f56dceca013cf9e6d16
Eric Kepes <ekepes@gmail.com>
2014-12-23 14:06:54 -0500

    A real start.

---
+++ src/mudsrv/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module mudsrv.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("mudsrv")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("eric")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ src/mudsrv/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "What is your name?"
+
+    let name = Console.ReadLine();
+    printfn "Hello, %s" name
+
+    0 // return an integer exit code
+
