4c3fd0831144d608d03f6a2b44dd48c1fc1a536d
Eric Kepes <ekepes@gmail.com>
2014-12-23 15:33:15 -0500

    Hey! It actually works. Not that it does much.

--- src/mudsrv/Program.fs
+++ src/mudsrv/Program.fs
@@ -1,40 +1,5 @@
 ﻿open System
-
-type Point = {
-    X: int;
-    Y: int
-    }
-
-type Character = {
-    Name: string;
-    Location: Point
-    }
-
-let AcceptCommand = 
-    printf "Enter command: "
-    let command = Console.ReadLine().ToLower();
-    match command with
-    | "north" -> Some({ X = 0; Y = -1 })
-    | "south" -> Some({ X = 0; Y = 1 })
-    | "east" -> Some({ X = 1; Y = 0 })
-    | "west" -> Some({ X = -1; Y = 0 })
-    | "quit" -> None
-    | _ -> Some({ X = 0; Y = 0 })
-
-let Move character delta : Character = 
-    let newCharacter = { 
-        Name = character.Name;
-        Location = { X = (character.Location.X + delta.X); Y = (character.Location.Y + delta.Y) }
-        }
-
-    newCharacter
-
-let GameLoop character =
-    let command = AcceptCommand
-
-    match command with
-    | Some delta -> Move character delta
-    | None -> character
+open EWK.MudSrv.Game
 
 [<EntryPoint>]
 let main argv = 
@@ -45,6 +10,6 @@ let main argv =
     let character = { Name = name; Location = { X = 0; Y = 0 } }
 
     let newCharacter = GameLoop character
-    printfn "Now at (%i, %i)." character.Location.X character.Location.Y
+    printfn "Now at (%i, %i)." newCharacter.Location.X newCharacter.Location.Y
 
     0 // return an integer exit code
---
+++ src/mudsrv/game.fs
@@ -0,0 +1,41 @@
+﻿namespace EWK.MudSrv
+
+module Game =
+
+    open System
+
+    type Point = {
+        X: int;
+        Y: int
+        }
+
+    type Character = {
+        Name: string;
+        Location: Point
+        }
+
+    let AcceptCommand = 
+        printf "Enter command: "
+        let command = Console.ReadLine().ToLower();
+        match command with
+        | "north" -> Some({ X = 0; Y = -1 })
+        | "south" -> Some({ X = 0; Y = 1 })
+        | "east" -> Some({ X = 1; Y = 0 })
+        | "west" -> Some({ X = -1; Y = 0 })
+        | "quit" -> None
+        | _ -> Some({ X = 0; Y = 0 })
+
+    let Move character delta : Character = 
+        let newCharacter = { 
+            Name = character.Name;
+            Location = { X = (character.Location.X + delta.X); Y = (character.Location.Y + delta.Y) }
+            }
+
+        newCharacter
+
+    let GameLoop character =
+        let command = AcceptCommand
+
+        match command with
+        | Some delta -> Move character delta
+        | None -> character
